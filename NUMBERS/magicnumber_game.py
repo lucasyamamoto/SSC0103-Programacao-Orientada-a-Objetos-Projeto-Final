6a509ddc12f2fad1bfd5d0c5b8706cb3f48f24f5
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2020-07-26 17:56:59 -0300

    Put Magic Numbers game into a class

--- NUMBERS/magicnumber_game.py
+++ NUMBERS/magicnumber_game.py
@@ -3,218 +3,219 @@ from pygame.locals import *
 import numpy as np
 from random import *
 
-N  = 3
-magic_square = np.zeros((N,N), dtype=int)
-
-aleatory = randint(0, 100)
-
-actualPlay = 1  + aleatory
-
-grid = [ [ None, None, None ], 
-         [ None, None, None ],
-         [ None, None, None ] ]
-
-win = None
-
-width = 600
-height = 525
-padding = 0
-
-def restartGame(board):
-    global magic_square,aleatory,N,actualPlay,win,grid
-    board.fill ((250, 250, 250))
-
-    distw = (int) (width/3) - padding
-    disth = (int) (height) - padding
-    
-    pygame.draw.line (board, (0,0,0), (distw, padding), (distw, disth), 2)
-    pygame.draw.line (board, (0,0,0), (2*distw,padding), (2*distw, disth), 2)
-
-    distw = (int) (width) - padding
-    disth = (int) (height/3) - padding
-
-    pygame.draw.line (board, (0,0,0), (padding, disth), (distw, disth), 2)
-    pygame.draw.line (board, (0,0,0), (padding, 2*disth), (distw, 2*disth), 2)
-
+class MagicNumber:
     N  = 3
-    magic_square = np.zeros((N,N), dtype=int)
-
-    grid = [ [ None, None, None ], 
-         [ None, None, None ],
-         [ None, None, None ] ]
-
-    aleatory = randint(0, 100)
-
-    actualPlay = 1  + aleatory
-
-    win = None
-
-
-def initBoard(ttt):
-
-    background = pygame.Surface (ttt.get_size())
-    background = background.convert()
-    background.fill ((250, 250, 250))
-
-    distw = (int) (width/3) - padding
-    disth = (int) (height) - padding
     
-    pygame.draw.line (background, (0,0,0), (distw, padding), (distw, disth), 2)
-    pygame.draw.line (background, (0,0,0), (2*distw,padding), (2*distw, disth), 2)
+    def __init__(self):
+        self.magic_square = np.zeros((self.N,self.N), dtype=int)
 
-    distw = (int) (width) - padding
-    disth = (int) (height/3) - padding
+        self.aleatory = randint(0, 100)
 
-    pygame.draw.line (background, (0,0,0), (padding, disth), (distw, disth), 2)
-    pygame.draw.line (background, (0,0,0), (padding, 2*disth), (distw, 2*disth), 2)
+        self.actualPlay = 1  + self.aleatory
 
-    return background
+        self.grid = [ [ None, None, None ], 
+                      [ None, None, None ],
+                      [ None, None, None ] ]
 
-def makesSquare():
-    global magic_square,aleatory
+        self.win = None
+
+        self.width = pygame.display.get_surface().get_width()
+        self.height = pygame.display.get_surface().get_height() - 200
+        self.padding = 0
+
+    def restartGame(self, board):
+        board.fill ((250, 250, 250))
+
+        distw = (int) (self.width/3) - self.padding
+        disth = (int) (self.height) - self.padding
     
-    n = 1
-    i, j = 0, N//2
+        pygame.draw.line (board, (0,0,0), (distw, self.padding), (distw, disth), 2)
+        pygame.draw.line (board, (0,0,0), (2*distw,self.padding), (2*distw, disth), 2)
 
-    while n <= N**2:
-        magic_square[i, j] = n
-        n += 1
-        newi, newj = (i-1) % N, (j+1)% N
-        if magic_square[newi, newj]:
-            i += 1
+        distw = (int) (self.width) - self.padding
+        disth = (int) (self.height/3) - self.padding
+
+        pygame.draw.line (board, (0,0,0), (self.padding, disth), (distw, disth), 2)
+        pygame.draw.line (board, (0,0,0), (self.padding, 2*disth), (distw, 2*disth), 2)
+
+        self.N  = 3
+        self.magic_square = np.zeros((self.N,self.N), dtype=int)
+
+        self.grid = [ [ None, None, None ], 
+                 [ None, None, None ],
+                 [ None, None, None ] ]
+
+        self.aleatory = randint(0, 100)
+
+        self.actualPlay = 1  + self.aleatory
+
+        self.win = None
+
+
+    def initBoard(self, ttt):
+
+        background = pygame.Surface (ttt.get_size())
+        background = background.convert()
+        background.fill ((250, 250, 250))
+
+        distw = (int) (self.width/3) - self.padding
+        disth = (int) (self.height) - self.padding
+    
+        pygame.draw.line (background, (0,0,0), (distw, self.padding), (distw, disth), 2)
+        pygame.draw.line (background, (0,0,0), (2*distw,self.padding), (2*distw, disth), 2)
+
+        distw = (int) (self.width) - self.padding
+        disth = (int) (self.height/3) - self.padding
+
+        pygame.draw.line (background, (0,0,0), (self.padding, disth), (distw, disth), 2)
+        pygame.draw.line (background, (0,0,0), (self.padding, 2*disth), (distw, 2*disth), 2)
+
+        return background
+
+    def makesSquare(self):
+        n = 1
+        i, j = 0, self.N//2
+
+        while n <= self.N**2:
+            self.magic_square[i, j] = n
+            n += 1
+            newi, newj = (i-1) % self.N, (j+1)% self.N
+            if self.magic_square[newi, newj]:
+                i += 1
+            else:
+                i, j = newi, newj
+
+        for i in range(3):
+            for j in range(3):
+                self.magic_square[i][j] += self.aleatory
+        print("Numero Aleatorio: " + str(self.aleatory))
+        print(self.magic_square)
+    
+    def drawStatus (self, board):
+
+        message1 = "A soma de cada linha será: " + str((3*self.aleatory)+15)
+        message2 = ""
+        if(self.win!=None):
+            if(self.win):
+                message2 = "Você Ganhou"
+            else:
+                message2 = "Você Perdeu"
         else:
-            i, j = newi, newj
+            message2 += "Clique no lugar onde irá o número " + str(self.actualPlay)
 
-    for i in range(3):
-        for j in range(3):
-            magic_square[i][j] += aleatory
-    print("Numero Aleatorio: " + str(aleatory))
-    print(magic_square)
+        fontsize = 45
+        font = pygame.font.Font(None, fontsize)
+        text1 = font.render(message1, True, (10, 10, 10))
+        text2 = font.render(message2, True, (10, 10, 10))
+
+        distw = (int) (10)
+        disth = (int) (self.height) + 50
+
+        board.fill ((250, 250, 250), (0, 500, 600, 200))
+        board.blit(text1, (distw, disth))
+        board.blit(text2, (distw, disth+50))
+        return
+
+    def showBoard (self, ttt, board):
+
+        self.drawStatus (board)
+        ttt.blit (board, (0, 0))
+        pygame.display.flip()
     
-def drawStatus (board):
+    def boardPos (self, mouseX, mouseY):
+        distw = (int) (self.width/3) - self.padding
+        disth = (int) (self.height/3) - self.padding
 
-    global win,actualPlay
-    message1 = "A soma de cada linha será: " + str((3*aleatory)+15)
-    message2 = ""
-    if(win!=None):
-        if(win):
-            message2 = "Você Ganhou"
+        if (mouseY < disth):
+            row = 0
+        elif (mouseY < 2*disth):
+            row = 1
         else:
-            message2 = "Você Perdeu"
-    else:
-        message2 += "Clique no lugar onde irá o número " + str(actualPlay)
+            row = 2
 
-    fontsize = 45
-    font = pygame.font.Font(None, fontsize)
-    text1 = font.render(message1, True, (10, 10, 10))
-    text2 = font.render(message2, True, (10, 10, 10))
+        if (mouseX < distw):
+            col = 0
+        elif (mouseX < 2*distw):
+            col = 1
+        else:
+            col = 2
 
-    distw = (int) (10)
-    disth = (int) (height) + 50
+        return (row, col)
 
-    board.fill ((250, 250, 250), (0, 500, 600, 200))
-    board.blit(text1, (distw, disth))
-    board.blit(text2, (distw, disth+50))
-    return
+    def drawMove (self, board, boardRow, boardCol, Piece):
+        distw = (int) (self.width/3) - self.padding
+        disth = (int) (self.height/3) - self.padding
 
-def showBoard (ttt, board):
+        centerX = (int) (((boardCol) * distw) + (distw/2))
+        centerY = (int) (((boardRow) * disth) + (disth/2))
+        fontsize = 90
+        font = pygame.font.Font(None, fontsize)
+        text = font.render(str(self.actualPlay), True, (10, 10, 10))
 
-    drawStatus (board)
-    ttt.blit (board, (0, 0))
-    pygame.display.flip()
+        #board.fill ((250, 250, 250), (0, 300, 300, 25))
+        board.blit(text, (centerX-20, centerY-20))
+
+        self.grid [boardRow][boardCol] = Piece
     
-def boardPos (mouseX, mouseY):
-    distw = (int) (width/3) - padding
-    disth = (int) (height/3) - padding
+    def clickBoard(self, board):
+        (mouseX, mouseY) = pygame.mouse.get_pos()
+        if(mouseY > self.height or mouseX > self.width):
+            return
 
-    if (mouseY < disth):
-        row = 0
-    elif (mouseY < 2*disth):
-        row = 1
-    else:
-        row = 2
+        (row, col) = self.boardPos(mouseX, mouseY)
 
-    if (mouseX < distw):
-        col = 0
-    elif (mouseX < 2*distw):
-        col = 1
-    else:
-        col = 2
+        if ((self.grid[row][col] != None)):
+            return
 
-    return (row, col)
-
-def drawMove (board, boardRow, boardCol, Piece):
-    global grid, actualPlay
-
-    distw = (int) (width/3) - padding
-    disth = (int) (height/3) - padding
-
-    centerX = (int) (((boardCol) * distw) + (distw/2))
-    centerY = (int) (((boardRow) * disth) + (disth/2))
-    fontsize = 90
-    font = pygame.font.Font(None, fontsize)
-    text = font.render(str(actualPlay), True, (10, 10, 10))
-
-    #board.fill ((250, 250, 250), (0, 300, 300, 25))
-    board.blit(text, (centerX-20, centerY-20))
-
-    grid [boardRow][boardCol] = Piece
+        print(str(row) +  " " + str(col))
     
-def clickBoard(board):
+        self.drawMove (board, row, col, self.actualPlay)
 
-    global grid, actualPlay
+        self.actualPlay+=1
     
-    (mouseX, mouseY) = pygame.mouse.get_pos()
-    if(mouseY > height or mouseX > width):
+    def gameWon(self, board):
+        if((self.actualPlay) < (self.aleatory+10) or self.win != None):
+            return
+
+        for i in range(3):
+            for j in range(3):
+                if(self.grid[i][j] != self.magic_square[i][j]):
+                    win = False
+                    return
+        self.win = True
         return
 
-    (row, col) = boardPos(mouseX, mouseY)
+    def main(self, ttt):
+        self.makesSquare()
 
-    if ((grid[row][col] != None)):
-        return
+        board = self.initBoard (ttt)
 
-    print(str(row) +  " " + str(col))
-    
-    drawMove (board, row, col, actualPlay)
+        running = True
 
-    actualPlay+=1
-    
-def gameWon(board):
-    global grid, win,actualPlay,magic_square
-
-    if((actualPlay) < (aleatory+10) or win != None):
-        return
-
-    for i in range(3):
-        for j in range(3):
-            if(grid[i][j] != magic_square[i][j]):
-                win = False
-                return
-    win = True
-    return
+        while running:
+            for event in pygame.event.get():
+                if event.type is QUIT:
+                    running = False
+                elif event.type is MOUSEBUTTONDOWN:
+                    self.clickBoard(board)
+                elif event.type is KEYDOWN:
+                    if event.key == pygame.K_r:
+                        self.restartGame(board)
+                        self.makesSquare()
+                
+                self.gameWon (board)
+                self.showBoard (ttt, board)
 
 # --------------------------------------------------------------------
-pygame.init()
-ttt = pygame.display.set_mode ((width,height + 200))
-pygame.display.set_caption ('Número Mágico')
-makesSquare()
 
-board = initBoard (ttt)
+if __name__ == '__main__':
+    pygame.init()
+    width = 600
+    height = 525
+    ttt = pygame.display.set_mode ((width,height + 200))
+    pygame.display.set_caption ('Número Mágico')
 
-running = 1
+    game = MagicNumber()
+    game.main(ttt)
 
-while (running == 1):
-    for event in pygame.event.get():
-        if event.type is QUIT:
-            running = 0
-        elif event.type is MOUSEBUTTONDOWN:
-            clickBoard(board)
-        elif event.type is KEYDOWN:
-            if event.key == pygame.K_r:
-                restartGame(board)
-                makesSquare()
-                
-        gameWon (board)
-        showBoard (ttt, board)
-
-pygame.quit()
+    pygame.quit()
